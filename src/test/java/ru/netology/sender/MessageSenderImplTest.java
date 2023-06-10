5c752570bd5c0671354595ab4957b6260a9d3f6d
jdvpgii <gii.jdvp@gmail.com>
2023-06-10 22:19:38 +0500

    test GeoServiceImplTest, MessageSenderImplTest

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -7,9 +7,7 @@ import org.mockito.Mockito;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
-import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
-import ru.netology.i18n.LocalizationServiceImpl;
 
 import java.util.HashMap;
 import java.util.Map;
