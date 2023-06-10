64b18a8283075218243e2bd289a0229d36c43a72
jdvpgii <gii.jdvp@gmail.com>
2023-06-10 21:32:57 +0500

    test LocalizationServiceImplTest

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -1,16 +1,18 @@
 package ru.netology.geo;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 /*
 Проверить работу метода public Location byIp(String ip)
  */
 class GeoServiceImplTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void byIp() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void byCoordinates() {
     }
 }
--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -1,12 +1,18 @@
 package ru.netology.i18n;
 
-import static org.junit.jupiter.api.Assertions.*;
-/*
-Проверить работу метода public String locale(Country country)
- */
-class LocalizationServiceImplTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
 
-    @org.junit.jupiter.api.Test
-    void locale() {
+class LocalizationServiceImplTest {
+    LocalizationService localizationService = new LocalizationServiceImpl();
+    @Test
+    void localeRu() {
+        Assertions.assertEquals(localizationService.locale(Country.RUSSIA), "Добро пожаловать");
+    }
+
+    @Test
+    void localeUSA() {
+        Assertions.assertEquals(localizationService.locale(Country.USA), "Welcome");
     }
 }
